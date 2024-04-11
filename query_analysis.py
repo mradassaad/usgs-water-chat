817ca16b86ffd254dfd8c2ca094998108b81a888
Assaad Mrad <mradassaad2@gmail.com>
2024-04-11 16:32:03 -0400

    remove redundant function

--- query_analysis.py
+++ query_analysis.py
@@ -47,35 +47,6 @@ def things_retrieval(search: ThingsSearch) -> dict:
     print(url)
     return response.json()
 
-def query_analysis(query: str) -> ThingsSearch:
-    """
-    Analyze a query to search over USGS SensorThings Things.
-    :param query: the query to analyze
-    :return: a ThingsSearch object
-    """
-
-    system = """You are an expert at converting user questions into USGS SensorThings API queries. \
-        Given a question or request, return a list of API queries optimized to retrieve the most relevant data. \
-        
-        If there are acronyms or words you are not familiar with, do not try to rephrase them.
-        """
-    
-    prompt = ChatPromptTemplate.from_messages(
-        [
-            ("system", system),
-            ("human", "{question}")
-        ]
-    )
-
-    llm = ChatOpenAI(
-        api_key=api_utils.get_openai_key_from_file("openai_key.txt"),
-        model=GPT_MODEL,
-        temperature=0)
-
-    structured_llm = llm.with_structured_output(ThingsSearch)
-    query_analyzer = {"question": RunnablePassthrough()} | prompt | structured_llm
-    
-    return query_analyzer.invoke(query) 
 
 def retrieval(query: str) -> dict:
     """
