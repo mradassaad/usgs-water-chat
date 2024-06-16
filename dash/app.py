bbdba1b405da99885dbe2627cc480aa1779b4732
Assaad Mrad <mradassaad2@gmail.com>
2024-06-16 17:10:27 -0400

    declare server

--- dash/app.py
+++ dash/app.py
@@ -5,7 +5,8 @@ import pandas as pd
 
 df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/gapminder_unfiltered.csv')
 
-app = Dash()
+app = Dash(__name__)
+server = app.server
 
 app.layout = [
     html.H1(children='Title of Dash App', style={'textAlign': 'center'}),
