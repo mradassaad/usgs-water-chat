0c10a83f48cf81b0ca197a707df496877b82405c
Assaad Mrad <mradassaad2@gmail.com>
2024-07-18 11:03:04 -0400

    Update scraping functionality and testing

--- knowledgebase_rag/utils.py
+++ knowledgebase_rag/utils.py
@@ -8,10 +8,37 @@ import datetime
 import logging
 from urllib.parse import urlparse
 
+# Function to check if a URL is valid.
+def is_valid_url(url: str) -> str:
+    """
+    Check if a URL is valid. If the URL is not valid, raise a ValueError. 
+    
+    Args:
+        url (str): A URL to check.
+
+    Returns:
+        str: A valid URL.
+    """
+
+    if url is None:
+        logging.warning("URL cannot be None.")
+        raise ValueError("URL cannot be None.")
+    elif not url.startswith("https://"):
+        logging.error("Invalid URL. Please provide a valid URL that starts with https://.")
+        raise ValueError("Invalid URL. Please provide a valid URL that starts with https://.")
+    elif any(char in url for char in ['<', '>', '"', "'", ';', '(', ')', '&', '|', '`']):
+        logging.error(f"Invalid character found in URL.")
+        raise ValueError(f"Invalid character found in URL.")
+    
+    if not url.endswith("/"):
+        url += "/"
+
+    return url
 
 # Function to find all pages with the same base URL.
 def find_pages_from_base(base_url: str, max_pages: int = 1000) -> list:
-    """Find all pages with the same base URL. Uses beautiful soup to scan the base page for child pages. Searches only one level of depth/
+    """Find all pages with the same base URL. Uses beautiful soup to scan the base page for child pages.
+    This function will recursively scan child pages for more child pages until the max_pages is reached or no new pages are found.
 
     Args:
         base_url (str): The base URL.
@@ -21,43 +48,62 @@ def find_pages_from_base(base_url: str, max_pages: int = 1000) -> list:
         list: A list of URLs.
     """
 
-    # Ensure base_url is a valid URL
-    if base_url is None:
-        logging.error("URL cannot be None.")
-        raise ValueError("URL cannot be None.")
-    elif not base_url.startswith("https://"):
-        logging.error("Invalid URL. Please provide a valid URL that starts with https://.")
-        raise ValueError("Invalid URL. Please provide a valid URL that starts with https://.")
-    elif any(char in base_url for char in ['<', '>', '"', "'", ';', '(', ')', '&', '|', '`']):
-        logging.error(f"Invalid character found in URL.")
-        raise ValueError(f"Invalid character found in URL.")
-    elif not base_url.endswith("/"):
-        base_url += "/"
+    # TODO: This function rigidely processes relatives URLs which may not work for some websites like https://learn.microsoft.com/en-us/azure/azure-functions/.
 
-    # Parse url
-    parsed_url = urlparse(base_url)
+    # Check if the URL is valid
+    base_url = is_valid_url(base_url)  
 
-    # Send a GET request to the webpage
-    page = requests.get(base_url)
-    # Parse the HTML content using BeautifulSoup
-    soup = BeautifulSoup(page.content, "html.parser")
-    links = soup.find_all("a")
-    pages = []
-    for link in links:
-        href = link.get("href")
+    # List to store pages to visit and set to store visited pages
+    visited = set()
+    to_visit = [base_url]
 
-        # Check if we have reached the maximum number of pages
-        if len(pages) >= max_pages:
+    def scrape(url):
+        # Parse url
+        parsed_url = urlparse(url)
+
+        # Send a GET request to the webpage
+        page = requests.get(url)
+        # Parse the HTML content using BeautifulSoup
+        soup = BeautifulSoup(page.content, "html.parser")
+        links = soup.find_all("a")
+        
+        for link in links:
+            href = link.get("href")
+
+            new_url = None
+            # Check if the href is a child page
+            if href is not None and (href.startswith(parsed_url.path)):
+                new_url = url.replace(parsed_url.path, "") + href
+            elif href is not None and (href.startswith(url)):
+                new_url = href
+            
+            # Disregard anchor urls
+            if new_url is not None and "#" in new_url:
+                new_url = None
+
+            # Check if new URL is valid
+            try:
+                new_url = is_valid_url(new_url)
+            except ValueError as e:
+                logging.debug(f"Skipping URL Error: {e}")
+                continue
+
+            if new_url is not None and new_url not in visited:
+                to_visit.append(new_url)
+
+    i = 0
+    while to_visit:
+        # increase counter
+        i += 1
+        logging.debug(f"Visiting URL: {to_visit[0]}")
+        # Check if counter is greater than max pages
+        if i > max_pages:
             logging.warning(f"Max pages reached: {max_pages}.")
             break
+        # Get the next page to visit
+        page = to_visit.pop(0)
+        if page not in visited:
+            visited.add(page)
+            scrape(page)
 
-        # Check if the href is a child page
-        if href is not None and (href.startswith(parsed_url.path)):
-            pages.append(base_url.replace(parsed_url.path, "") + href)
-        elif href is not None and (href.startswith(base_url)):
-            pages.append(href)
-
-        # keep only unique pages
-        pages = list(set(pages))
-
-    return pages
+    return list(visited)
--- tests/test_knowledgebase_rag_utils.py
+++ tests/test_knowledgebase_rag_utils.py
@@ -2,10 +2,33 @@ import unittest
 import knowledgebase_rag.utils as utils
 
 class TestKnowledgeRAGUtils(unittest.TestCase):
+
+    def test_is_valid_url_valid(self):
+        # Test with a valid URL, check if return type is string
+        url = utils.is_valid_url("https://waterservices.usgs.gov/docs/")
+        self.assertIsInstance(url, str)
+
+        # Test with a valid URL, check if the returned URL ends with a "/"
+        url = utils.is_valid_url("https://waterservices.usgs.gov/docs")
+        self.assertTrue(url.endswith("/"))
+
+    def test_is_valid_url_invalid(self):
+        # Test with an invalid URL, check if ValueError is raised
+        with self.assertRaises(ValueError):
+            utils.is_valid_url("waterservices.usgs.gov/docs/")
+        # Test with an invalid URL, check if ValueError is raised
+        with self.assertRaises(ValueError):
+            utils.is_valid_url("https://waterservices.usgs.gov/docs/'; DROP TABLE users;")
+        # Test with None, check if ValueError is raised
+        with self.assertRaises(ValueError):
+            utils.is_valid_url(None)
+
     def test_find_pages_from_base_valid(self):
-        # Test with a valid URL
+        # Test with USGS URL, check if return type is list
         links = utils.find_pages_from_base("https://waterservices.usgs.gov/docs/")
         self.assertIsInstance(links, list)
+        # Test with USGS URL, check if the length of the list is greater than 0
+        self.assertGreater(len(links), 0)
         
     def test_find_pages_from_base_invalid(self):
         # Test with an invalid URL
